3f974c0d0c724d4a4b113732fbe4830683cb5ac4
chillkoala125 <113270114+chillkoala125@users.noreply.github.com>
2022-12-17 12:40:53 -0500

    Update BakeryItem.js

--- src/BakeryItem.js
+++ src/BakeryItem.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+//import Button from 'react-bootstrap/Button';
+//import Card from 'react-bootstrap/Card';
 // TODO: create a component that displays a single bakery item
 export default function BakeryItem({ image, name, desc, price, size, time, cartTotal, addPrice, addItem, items }) {
     const [count, increment] = useState(0);
@@ -11,18 +11,14 @@ export default function BakeryItem({ image, name, desc, price, size, time, cartT
       }
 
   return (
-          <Card border="secondary" style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={image} width="200" />
-                <Card.Body>
-          <Card.Title>{name}</Card.Title>
-                  <Card.Text>
+          <div>
+          <img src={image} width="200"/>
+          <h5>{name}</h5>
           <p>{desc}</p>
           <p>{price}</p>
           <p>{size}</p>
           <p>{time}</p>
-                  </Card.Text>
-          <Button variant="primary" onClick={handleClick}>Add to Cart</Button>
-                </Card.Body>
-              </Card>
+          <button onClick={handleClick}>Add to Cart</button>
+          </div>
   );
 }
