c2ed8cb0c8fd4698e096ac55f8a613c836aec5b5
chillkoala125 <113270114+chillkoala125@users.noreply.github.com>
2022-12-16 22:14:24 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import bakeryData from "./bakery-data.json";
+import BakeryItem from "./BakeryItem.js"
+
+/* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
+bakeryData.forEach((item) => {
+  item.image = process.env.PUBLIC_URL + "/" + item.image;
+});
+/* ############################################################## */
 
 function App() {
+  // TODO: use useState to create a state variable to hold the state of the cart
+  /* add your cart state code here */
+    const [cartTotal, addPrice] = useState(0);
+    const [items, addItem] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+          
+          
+    
+      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+            <BakeryItem image={item.image}
+                        name= {item.name}
+                        desc={item.description}
+                        price={item.price}
+                        size={item.size}
+                        cartTotal={cartTotal}
+                        addPrice={addPrice}
+                        items={items}
+                        addItem={addItem}/>
+                                        
+      ))}
+
+      <div>
+        <h2>Cart</h2>
+        {/* TODO: render a list of items in the cart */}
+          {items.map((item) => (
+              <p>{item.name}</p>
+          ))}
+          <p>Total: ${cartTotal}</p>
+      </div>
     </div>
   );
 }
